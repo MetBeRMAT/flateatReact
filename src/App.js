a87adc8b1a42a0867b40f613b2c98e91dbc4ba3e
Mattia Bernardi <bernardi.mattia@libero.it>
2024-02-27 12:17:43 +0100

    aggiunta pagina restaurant bozza

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './navbar';
 import Login from './login';
 import 'bootstrap/dist/css/bootstrap.css';
+import Restaurant from './Restaurant';
+import { atom, useAtom } from 'jotai';
 
-function App() {
+export const currentUser = atom();
+
+function App() 
+{
+
+  //const [user,setUser] = useAtom(currentUser);
+
   return (
     <>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-      <Route path='login' element={<Login />}> </Route>
-      </Routes>
+        <Navbar />
+          <Routes>
+            <Route path='login' element={<Login />}> </Route>
+            <Route path='restaurant' element={<Restaurant />}> </Route>
+          </Routes>
       </BrowserRouter>
     </>
   );
--- src/navbar.js
+++ src/navbar.js
@@ -6,14 +6,14 @@ export default function navbar()
         <>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Home</a>
+                    <a class="navbar-brand" to="/">Home</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <Link class="nav-link active" aria-current="page" to="/login">Login</Link>
-                        <a class="nav-link" href="#">Features</a>
+                        <Link class="nav-link active" aria-current="page" to="/restaurant">Restaurant</Link>
                         <a class="nav-link" href="#">Pricing</a>
                         <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                     </div>
