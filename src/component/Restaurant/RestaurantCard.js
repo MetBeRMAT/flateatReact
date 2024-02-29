bfbbf66a7a92bc451c34980026a6474333091894
niko Ricci <MY_NAssME@example.com>
2024-02-29 15:54:20 +0100

    front nikk v44

--- src/component/Restaurant/RestaurantCard.js
+++ src/component/Restaurant/RestaurantCard.js
@@ -1,69 +1,52 @@
 import { Link } from "react-router-dom";
 import FoodCard from "./FoodCard";
 
-export default function RestaurantCard(props)
-{
 
-    function Card({ foodTypes,open ,name, phone, x, y, distance}) 
-    {
+export default function RestaurantCard(props) {
 
-        return (
-            // la visualizzo in colonne da tre ed ho impostato lo stato isOpen e tutti i tipi di cibi associati
-            // Solo che bisogna dare una controllata a isOpen dato che mi da sempre Closed 
-
-                <div class="card text-bg-success">
-                        <h3 style={{fontFamily:"Lucida Handwriting,cursive"}} class="card-title">Restaurant {name}</h3>
-                        <dl class="row">
-                            <dt class="col-sm-3"></dt>
-                            <dt style={{fontFamily:"Times New Roman,Serif"}}>Type of food: {foodTypes.map(f => <FoodCard name={f} />)}</dt>
-                            <dt class="col-sm-3"></dt>
-                            <dd class="col-sm-9">
-                                <p></p>
-                                <p></p>
-                            </dd>
-
-                            <dt style={{fontFamily:"Times New Roman,Serif"}} class="col-sm-4">{open ? "Open" : "Closed"}</dt>
-                            {distance!=null ? <div> {distance} </div> : <></>}
-                        </dl>
-                        {/* <button class="btn btn-info position-absolute bottom-0 end-0" type="button"><Link class="nav-link" to={"/RestaurantDetail/"+title}>Details</Link></button> */}
-                
-            </div>
-        );
-    }
-
-    function CardGrid() 
-    {
-        return (
-
-            <div className="row row-cols-2 g-4">
-                <Card distance={props.distance} foodTypes={props.foodTypes} open={props.isOpen} name={props.name} />
-            </div>
-        );
-    }
-
-    return(
-        <>
-            {/* ho richiamato la CardGrid di sopra ho solo richiamato la funzione senza che visualizzi una nuova card dentro il return */}
-
-
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <CardGrid/>
-                        </div>
-                    </div>
-                </div>
-        {/* <dl class="row text-bg-success m-3">
-            <dt class="col-sm-3"></dt>
-                <dd class="col-sm-9">
-                    <p></p>
-                    <p></p>
-                </dd>
-            <dt style={{fontFamily:"Times New Roman,Serif"}} class="col-sm-3">Restaurant: {props.name}</dt>
-            <dt class="col-sm-3"></dt>
-            <dd style={{fontFamily:"Times New Roman,Serif"}} class="col-sm-3">WeFlat are on: ({props.positionX} - {props.positionY})</dd>
-            <p><button class="btn btn-info " type="button"><Link class="nav-link" to={"/restaurantdetail/"+props.id}>Details</Link></button></p>
-        </dl> */}
-        </>
+  function Card({ foodTypes, open, name, phone, x, y, distance }) {
+    return (
+      <div className="card text-bg-success">
+        <Link to={`/restaurant/${props.id}`} className="text-decoration-none">
+          <div className="card text-bg-success">
+            <h3 style={{ fontFamily: "Lucida Handwriting,cursive" }} className="card-title">Restaurant {name}</h3>
+            <dl className="row">
+              <dt className="col-sm-3"></dt>
+              <dt style={{ fontFamily: "Times New Roman,Serif" }}>Type of food: {foodTypes.map(f => <FoodCard key={f} name={f} />)}</dt>
+              <dt className="col-sm-3"></dt>
+              <dd className="col-sm-9">
+                <p></p>
+                <p></p>
+              </dd>
+              <dt style={{ fontFamily: "Times New Roman,Serif" }} className="col-sm-4">{open ? "Open" : "Closed"}</dt>
+              {distance != null ? <div> {distance} </div> : <></>}
+            </dl>
+          </div>
+        </Link>
+        <Link to={`/order/${props.id}`} className="btn btn-danger">
+          Ordina ora
+        </Link>
+      </div>
     );
+  }
+
+  function CardGrid() {
+    return (
+      <div className="row row-cols-2 g-4">
+        <Card distance={props.distance} foodTypes={props.foodTypes} open={props.isOpen} name={props.name} />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <CardGrid />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
