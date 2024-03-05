e2630611ced64865d650544747c4ae49a5299b43
MetBeRMAT <bernardi.mattia2000@libero.it>
2024-03-05 11:23:24 +0100

    a

--- src/component/User/Register.js
+++ src/component/User/Register.js
@@ -50,7 +50,7 @@ export default function Register()
                     positionY:""
                 })
             }
-        ).then(navigate("/restaurantlogged"))
+        ).then(navigate("/"))
     }
 
     function checkRegister()
